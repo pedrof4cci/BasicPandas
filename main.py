9211c93a300c5395a488682c277300ccc85890cd
Pedro F <4pedro.facci@gmail.com>
2025-05-23 05:55:09 -0300

    Realizado implementação de filtros

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ dados = pd.read_csv('aluguel.csv', sep=';') #chama a base de dados
 base_tratada = dados.fillna(0)  #troca os valores null por 0
 registros_a_remover = base_tratada.query('Valor == 0 | Condominio == 0').index
 base_tratada.drop(registros_a_remover, axis= 0, inplace= True)  #romove as linhas com valor = 0 e condominio = 0
+
+tipo_imovel = input('Olá! Para começar nossa busca, me diga qual tipo de imóvel você procura: ')
+valor_imovel = int(input('Até qual valor você está procurando? '))
+
+filtro = (base_tratada['Tipo'] == tipo_imovel) & (base_tratada['Valor'] <= valor_imovel)
+print(filtro)
